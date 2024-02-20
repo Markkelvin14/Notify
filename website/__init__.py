1938fa7513fc722a474ee1c11b5578c1acd91338
Markkelvin14 <122996163+Markkelvin14@users.noreply.github.com>
2024-02-20 03:18:06 -0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,8 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'SECRET'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
 
     from .views import views
